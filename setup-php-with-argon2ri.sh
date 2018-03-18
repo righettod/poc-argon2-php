70412716ffbc3204675653d6ecd579963ff77f49
righettod <dominique.righetto@gmail.com>
2018-03-18 10:06:54 +0100

    Fix install script error

--- setup-php-with-argon2ri.sh
+++ setup-php-with-argon2ri.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 export CDIR=`pwd`
+ARGON2_RELEASE_NAME=20171227
+PHP_RELEASE_NAME=7.2.3
 echo "#### Install Argon2 from PHC release on Github repository ####"
 cd $CDIR
-RELEASE_NAME=20171227
-wget https://github.com/P-H-C/phc-winner-argon2/archive/$RELEASE_NAME.zip
-unzip $RELEASE_NAME.zip
-cd phc-winner-argon2-$RELEASE_NAME
+wget https://github.com/P-H-C/phc-winner-argon2/archive/$ARGON2_RELEASE_NAME.zip
+unzip $ARGON2_RELEASE_NAME.zip
+cd phc-winner-argon2-$ARGON2_RELEASE_NAME
 make
 make test > tests-argon2-library.txt
 TESTS_CONTAINS_ERROR=`grep -c FAIL tests-argon2-library.txt`
@@ -17,11 +18,11 @@ sudo make install
 ls -l /usr/lib/*argon2*
 echo "#### Install PHP 7.2 from sources with Argon2 option enabled ####"
 cd $CDIR
-wget http://de2.php.net/get/php-7.2.3.tar.gz/from/this/mirror
+wget http://de2.php.net/get/php-$PHP_RELEASE_NAME.tar.gz/from/this/mirror
 mv mirror mirror.tgz
 tar xf mirror.tgz
-cd php-7.2.3
+cd php-$PHP_RELEASE_NAME
 ./configure --with-password-argon2=/usr/lib --with-openssl --enable-mbstring --enable-dom --enable-json --enable-xml --enable-libxml
 make
-make test
+# make test
 sudo make install
